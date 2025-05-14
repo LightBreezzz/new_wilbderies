78435db70d9aedb35a656356546d7753c6dff2f1
Nikolai <kolya_fedosov1992@mail.ru>
2025-05-14 13:12:32 +0300

    add app

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "/media"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
